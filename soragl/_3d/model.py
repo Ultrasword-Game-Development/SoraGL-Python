a6331a423acc7b11b0bc8773827aab88c2afaddb
Peter Zhang <ultraswordkirbypink@gmail.com>
2023-02-15 22:08:15 -0500

    started parsing obj files -- parse mtl + create objects

--- soragl/_3d/model.py
+++ soragl/_3d/model.py
@@ -83,33 +83,82 @@ class MTLObjLoader(Loader):
             self._textures.create_and_add_texture(f)
         # config for mtl file
         self._config = {}
+        # results
+        self._results = {}
+
+    @property
+    def results(self):
+        """Get results"""
+        return self._results
+
+    @property
+    def objects(self):
+        """Get all object names"""
+        return list(self._results.keys())
 
     def load(self):
         """Load the mtl + obj file data"""
         # load obj file -- http://web.cse.ohio-state.edu/~shen.94/581/Site/Lab3_files/Labhelp_Obj_parser.htm
-
         with open(self._obj, "r") as file:
             data = file.read()
+
+        # data
+        objects = {}
+        current_object = None
+        reference = None
+
         for line in data.split("\n"):
             if not line or line[0] == "#":
                 continue
             if line.startswith("mtllib"):
                 # load mtl file
-                self.load_mtl(os.path.join(self.path, line.split()[1]))
+                self.load_mtl(os.path.join(self._path, line.split()[1]))
             # parsing
             if line[0] == "o":
-                pass
+                # if has old
+                if current_object:
+                    objects[current_object] = reference
+                # make new
+                current_object = line.split()[1]
+                # new obj -- 0 = vertices, 1 = uv, 2 = normal
+                reference = ([], [], [], [])
+                objects[current_object] = reference
             elif line[0] == "v":
-                pass
+                reference[0].append(list(map(float, line.split()[1:])))
             elif line.startswith("vt"):
-                pass
+                reference[1].append(list(map(float, line.split()[1:])))
             elif line.startswith("vn"):
-                pass
-            print(line)
+                reference[2].append(list(map(float, line.split()[1:])))
+            elif line.startswith("f"):
+                # vertex/texture/normal -- texture + normal are optional
+                # -1 == does not exist
+                face = []
+                for x in line.split()[1:]:
+                    r = []
+                    for i in x.split("/") + [""] * (3 - len(x.split("/"))):
+                        if i == "":
+                            continue
+                        r.append(int(i))
+                    face.append(r)
+                reference[3].append(face)
+            # print(line)
+        print(objects)
+        # finalizing data
+        # create vao + ibo
+        for name, vals in objects.items():
+            # generate final objects
+            vbuf = []
+            ibuf = []
+            for f in vals[3]:
+                buf += 
+
+            parse = f"{len(buf)}f"
+            _vertex_buf = mgl.Buffer(parse, vbuf)
+            _index_buf = mgl.Buffer(parse, ibuf)
 
     def load_mtl(self, path: str):
         """Load an mtl file"""
         # load mtl file
-        with open(self._mtl, "r") as file:
+        with open(path, "r") as file:
             # load data
             pass

f665f50a3521a3febbe82ef16eaae6025ba1e93e
mynameispet <ultraswordkirbypink@gmail.com>
2023-02-27 14:07:37 -0500

    base mac

---
+++ mac.py
@@ -0,0 +1,140 @@
+import pygame
+import soragl
+import struct
+
+from pygame import draw as pgdraw
+from pygame import math as pgmath
+from soragl import animation, scene, physics, base_objects
+
+# ------------------------------ #
+# setup
+SORA = soragl.SoraContext.initialize(
+    {
+        "fps": 30,
+        "window_size": [1280, 720],
+        "window_flags": pygame.RESIZABLE | pygame.DOUBLEBUF,
+        "window_bits": 32,
+        "framebuffer_flags": pygame.SRCALPHA,
+        "framebuffer_size": [1280 // 3, 720 // 3],
+        "framebuffer_bits": 32,
+        "debug": True,
+    }
+)
+
+SORA.create_context()
+
+# ------------------------------ #
+
+from scripts.entities import tomato
+
+# ------------------------------ #
+
+
+animation.Category.load_category("assets/sprites/tomato.json")
+registry = animation.Category.get_category_framedata("assets/sprites/tomato.json")[
+    "idle"
+].get_registry()
+
+
+image = SORA.load_image("assets/sprites/tomato.png")
+potato = SORA.load_image("assets/sprites/potato-2.png")
+__ss = animation.SpriteSheet(image, 16, 16, 0, 0)
+
+sc = scene.Scene(config=scene.load_config(scene.Scene.DEFAULT_CONFIG))
+scw = sc.make_layer(sc.get_config(), 1)
+scw.get_chunk(0, 0)
+
+sce1 = physics.Entity()
+sce2 = physics.Entity()
+sce3particle = physics.ParticleHandler(
+    create_func=physics.ParticleHandler.DEFAULT_CREATE,
+    update_func=physics.ParticleHandler.DEFAULT_UPDATE,
+)
+sce3particle.position += (100, 100)
+sce4particle = physics.ParticleHandler(create_func="square", update_func="square")
+sce4particle.position += (200, 100)
+sce4particle["interval"] = 0.5
+# sce5particle = physics.ParticleHandler(create_func="triangle", update_func="triangle")
+sce5particle = physics.ParticleHandler(create_func="custom", update_func="custom")
+sce5particle.position += (200, 150)
+sce5particle["interval"] = 0.5
+
+# add entities to world first
+scw.add_entity(sce1)
+scw.add_entity(sce2)
+scw.add_entity(sce3particle)
+scw.add_entity(sce4particle)
+scw.add_entity(sce5particle)
+scw.add_entity(tomato.Tomato(position=(50, 100)))
+
+# entity comp
+sce1.add_component(base_objects.Sprite(0, 0, potato))
+sce1.add_component(base_objects.SpriteRenderer())
+sce1.add_component(base_objects.Collision2DComponent())
+sce1.position += (100, 100)
+sce1.area = (20, 20)
+# sce1.static = True
+
+sce2.add_component(base_objects.AnimatedSprite(0, 0, registry))
+sce2.add_component(base_objects.SpriteRenderer())
+sce2.position += (40, 100)
+sce2.area = (16, 16)
+sce2.static = True
+
+# physics
+sce1.add_component(base_objects.Collision2DComponent())
+sce2.add_component(base_objects.Collision2DComponent())
+
+
+# aspects
+
+# scw.add_aspect(base_objects.TileMap())
+scw.add_aspect(base_objects.TileMapDebug())
+
+scw.add_aspect(base_objects.SpriteRendererAspect())
+# scw.add_aspect(base_objects.SpriteRendererAspectDebug())
+# scw.add_aspect(base_objects.Collision2DAspect())
+scw.add_aspect(base_objects.Collision2DRendererAspectDebug())
+scw.add_aspect(base_objects.RenderableAspect())
+
+
+scene.SceneHandler.push_scene(sc)
+
+# ------------------------------ #
+# post testing
+
+tm = scw.get_aspect(base_objects.TileMapDebug)
+tm.set_sprite_data("assets/sprites/shovel.png", pygame.Rect(0, 0, 16, 16))
+
+tm.add_tile_global("assets/sprites/shovel.png", 0, 0)
+tm.add_tile_global("assets/sprites/shovel.png", 17, 6)
+tm.add_tile_global("assets/sprites/shovel.png", 2, 3)
+tm.add_tile_global("assets/sprites/shovel.png", -2, 2)
+tm.add_tile_global("assets/sprites/shovel.png", 2, 2)
+
+
+# ------------------------------ #
+# game loop
+SORA.start_engine_time()
+while SORA.RUNNING:
+    # SORA.FRAMEBUFFER.fill((255, 255, 255, 255))
+    SORA.FRAMEBUFFER.fill((0, 0, 0, 255))
+    SORA.DEBUGBUFFER.fill((0, 0, 0, 0))
+    # pygame update + render
+    registry.update()
+    scene.SceneHandler.update()
+
+    if SORA.is_key_clicked(pygame.K_d) and SORA.is_key_pressed(pygame.K_LSHIFT):
+        SORA.DEBUG = not SORA.DEBUG
+
+    # push frame
+    SORA.push_framebuffer()
+    pygame.display.flip()
+    # update events
+    SORA.update_hardware()
+    SORA.handle_pygame_events()
+    # clock tick
+    SORA.CLOCK.tick(SORA.FPS)
+    SORA.update_time()
+
+pygame.quit()
